941132eb835e799f97626e55c05723685755bd01
Daniel Quest <daniel.quest@gmail.com>
2012-02-17 14:35:52 -0500

    Changing the port to the default port so that sys.properties agrees with this script

--- scripts/start_mongo.bash
+++ scripts/start_mongo.bash
@@ -5,4 +5,4 @@ sudo chmod 777 mongo/ ;
 cd mongo ;
 wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.2.tgz ;
 tar -xzvf mongodb-linux-x86_64-2.0.2.tgz ;
-nohup /mnt/mongo/mongodb-linux-x86_64-2.0.2/bin/mongod --port 443 --dbpath /mnt/mongo &
+nohup /mnt/mongo/mongodb-linux-x86_64-2.0.2/bin/mongod --port 27017 --dbpath /mnt/mongo &
