3f701cede26a047238b0116bd77d9890a5833843
Laura Quest <lauraquest@lauraanddan>
2012-05-02 15:39:56 -0400

    got the find script to work with the modified find semantics

--- cli/export.bash
+++ cli/export.bash
@@ -8,5 +8,5 @@ export PS_UPLOAD_PORT=7039
 echo Upload Port: $PS_UPLOAD_PORT
 export PS_USR=danielquest
 echo User: $PS_USR 
-export PS_WORKSPACE=w0aec977251dd464412abcfedc7edb971d18a0868
+export PS_WORKSPACE=w866575bd535432222e4330c8599830ad69db03df
 echo Workspace: $PS_WORKSPACE
